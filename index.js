738308fda20e41aab178fce458c6c21187a73a3e
seanwascoding <a225108347171@gmail.com>
2023-08-17 21:00:30 +0800

    first_version

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const path = require('path')
+const mysql = require('mysql')
+const app = express()
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const fs = require('fs')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.listen(8080, () => console.log("server opening"))
+
+//* mysql setup
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Amks94884674?',
+    database: 'test'
+});
+
+//* working login
+app.post('/info', (req, res) => {
+    console.log(req.body)
+    pool.query('select * from account where username = ?', [req.body.user], (err, results) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        console.log(results)
+        res.send(results)
+    })
+})
+
+//* upload file
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        fs.mkdir('./file', { recursive: true }, (err) => {
+            if (err) {
+                cb(err);
+            } else {
+                cb(null, './file');
+            }
+        });
+        cb(null, './file')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({ storage: storage })
+app.post('/file', upload.single('file'), (req, res) => {
+    console.log(req.headers['content-type'])
+    console.log(req.file)
+    res.send('work')
+})
