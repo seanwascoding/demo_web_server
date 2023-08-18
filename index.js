000a8c34d6deb41dc54e1fa412a34032ef7bef2c
seanwascoding <a225108347171@gmail.com>
2023-08-18 14:57:41 +0800

    Reload按鈕和下載功能

--- index.js
+++ index.js
@@ -42,16 +42,47 @@ const storage = multer.diskStorage({
                 cb(null, './file');
             }
         });
-        cb(null, './file')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
 });
-
 const upload = multer({ storage: storage })
 app.post('/file', upload.single('file'), (req, res) => {
     console.log(req.headers['content-type'])
     console.log(req.file)
     res.send('work')
 })
+
+//* admin can add model to dir => client can download it
+app.get('/reload', (req, res) => {
+    promise()
+        .then(result => res.send(result))
+        .catch(err => res.send(err))
+})
+function promise() {
+    return new Promise((resolve, reject) => {
+        const file_path = './model'
+        fs.readdir(file_path, (err, files) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(files)
+        })
+    })
+}
+app.get('/download/:filename', (req, res) => {
+    console.log(req.params.filename)
+    const filename = req.params.filename
+    const path2file = path.join('model', filename)
+    console.log(path2file, filename)
+    res.download(path2file, filename, (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log('send work')
+        }
+    })
+}) 
